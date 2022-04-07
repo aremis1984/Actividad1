e1b1b60286b06d933b4f161c62aa8ca407001d0a
marinaLeon84 <86837785+marinaLeon84@users.noreply.github.com>
2022-04-08 01:11:35 +0200

    Segunda activity, traducciones y landscape

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var outputTextView: TextView
     private var dotCLicked :Boolean=false
     lateinit var shareBtn: Button
-    var numClicked: Boolean= false
-    var opClicked: Boolean= false
-    var finalResult: String=""
+    private var numClicked: Boolean= false
+    private var opClicked: Boolean= false
+    private var finalResult: String=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        shareBtn = findViewById(R.id.compartir)
+        shareBtn.setOnClickListener {
+            val intent = Intent(this, ShareActivity::class.java)
+            intent.putExtra("final_result", finalResult)
+            startActivity(intent)
+        }
     }
 
     fun delete(view: View)
@@ -42,12 +48,14 @@ class MainActivity : AppCompatActivity() {
             outputTextView.text = textChain
             shareBtn = findViewById(R.id.compartir)
             shareBtn.isEnabled = false
+            opClicked = false
         }
     }
 
     fun operator (view: View)
     {
-        if (numClicked && !dotCLicked) {
+        var textChain = outputTextView.text.toString()
+        if (!dotCLicked && (numClicked || !opClicked && textChain.isNotEmpty())) {
             outputTextView = findViewById(R.id.resultado)
             outputTextView.append((view as Button).text)
             numClicked = false
@@ -101,15 +109,5 @@ class MainActivity : AppCompatActivity() {
                 dotCLicked = false
             }
         }
-    }
-
-    fun shareResult(view: View)
-    {
-        shareBtn = findViewById(R.id.compartir)
-        val intent= Intent()
-        intent.action=Intent.ACTION_SEND
-        intent.putExtra(Intent.EXTRA_TEXT,finalResult)
-        intent.type="text/plain"
-        startActivity(Intent.createChooser(intent,"Share To:"))
     }
 }
---
+++ app/src/main/java/com/example/myapplication/ShareActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ShareActivity : AppCompatActivity() {
+    private var result: String=""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share)
+        val bundle :Bundle ?=intent.extras
+        if (bundle !== null){
+            result = bundle.getString("final_result").toString()
+            val resultView = findViewById<TextView>(R.id.result_to_share)
+            resultView.text = result
+        }
+    }
+
+    fun shareResult(view: View)
+    {
+        val intent= Intent()
+        intent.action=Intent.ACTION_SEND
+        intent.putExtra(Intent.EXTRA_TEXT, result)
+        intent.type="text/plain"
+        startActivity(Intent.createChooser(intent,"Share To:"))
+    }
+
+    fun back(view: View)
+    {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
